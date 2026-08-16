c27c9da810908e52056f26cf17fdb3dde4dcca79
madhur25scs1003003148-ship-it <madhur.25scs1003003148@iilm.edu>
2026-08-17 00:02:33 +0530

    Create EmailInput.java

---
+++ EmailInput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EmailInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Email: ");
+        String email = sc.nextLine();
+
+        System.out.println("\nStudent Details");
+        System.out.println("----------------");
+        System.out.println("Name  : " + name);
+        System.out.println("Email : " + email);
+
+        sc.close();
+    }
+}
